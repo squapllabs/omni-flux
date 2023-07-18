1cb9dd722f92bde62cca51ab0ae3280961fdb96b
Squapl Technologies <squaplapps@squapl.com>
2023-07-18 10:09:59 +0530

    latest....

--- apps/web/src/hoc/withLayoutAndProtection.tsx
+++ apps/web/src/hoc/withLayoutAndProtection.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
+
 import Layout from '../layout/layout';
-import ProtectedRoute from '../auth/ProtectedRoute';
+import ProtectedRoute from '../routes/ProtectedRoute';
 
 // This is a higher-order component that wraps the provided Component in
 // a Layout and ProtectedRoute
