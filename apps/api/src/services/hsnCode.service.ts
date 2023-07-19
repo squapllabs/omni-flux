2df30f65f40ca49bd00855428d509ff5cb732df9
squaplapps <squaplapps@squapl.com>
2023-07-19 13:06:57 +0530

    merge iss

--- apps/api/src/services/hsnCode.service.ts
+++ apps/api/src/services/hsnCode.service.ts
@@ -1,83 +1,150 @@
-import axiosinterceptor from '../helper/custom_axios';
-import { environment } from '../environment/environment';
+import hsnCodeDao from '../dao/hsnCode.dao';
+import {
+  createHsnCodeBody,
+  updateHsnCodeBody,
+} from '../interfaces/hsnCode.Interface';
 
+/**
+ * Method to Create a New HsnCode
+ * @param body
+ * @returns
+ */
+const createHsnCode = async (body: createHsnCodeBody) => {
+  try {
+    const { code, description, created_by = null } = body;
+    const hsnCodeDetails = await hsnCodeDao.add(code, description, created_by);
+    const result = { success: true, data: hsnCodeDetails };
+    return result;
+  } catch (error) {
+    console.log('Error occurred in hsnCode service Add: ', error);
+    throw error;
+  }
+};
+
+/**
+ * Method to Update an Existing HsnCode
+ * @param body
+ * @returns
+ */
+const updateHsnCode = async (body: updateHsnCodeBody) => {
+  try {
+    const { code, description, updated_by, hsn_code_id } = body;
+    let result = null;
+    const hsnCodeExist = await hsnCodeDao.getById(hsn_code_id);
+    if (hsnCodeExist) {
+      const hsnCodeDetails = await hsnCodeDao.edit(
+        code,
+        description,
+        updated_by,
+        hsn_code_id
+      );
+      result = { success: true, data: hsnCodeDetails };
+      return result;
+    } else {
+      result = { success: false, message: 'hsn_code_id not exist' };
+      return result;
+    }
+  } catch (error) {
+    console.log('Error occurred in hsnCode service Edit: ', error);
+    throw error;
+  }
+};
+
+/**
+ * Method to get HsnCode By HsnCodeId
+ * @param hsnCodeId
+ * @returns
+ */
+const getById = async (hsnCodeId: number) => {
+  try {
+    let result = null;
+    const hsnCodeData = await hsnCodeDao.getById(hsnCodeId);
+    if (hsnCodeData) {
+      result = { success: true, data: hsnCodeData };
+      return result;
+    } else {
+      result = { success: false, message: 'hsn_code_id does not exist' };
+      return result;
+    }
+  } catch (error) {
+    console.log('Error occurred in getById hsnCode service : ', error);
+    throw error;
+  }
+};
+
+/**
+ * Method to Get All HsnCode's
+ * @returns
+ */
 const getAllHsnCode = async () => {
   try {
-    const response = await axiosinterceptor.get(
-      `${environment.apiUrl}/hsn-code/getAll`
-    );
-    return response.data;
+    const result = await hsnCodeDao.getAll();
+    const hsnCodeData = { success: true, data: result };
+    return hsnCodeData;
   } catch (error) {
-    console.log('Error in getting all hsn code data:', error);
+    console.log('Error occurred in getAllHsnCode hsnCode service : ', error);
     throw error;
   }
 };
 
-const deleteHsnCode = async (id: number) => {
+/**
+ * Method to delete hsnCode
+ * @param hsnCodeId
+ */
+const deleteHsnCode = async (hsnCodeId: number) => {
   try {
-    const response = await axiosinterceptor.delete(
-      `${environment.apiUrl}/hsn-code/delete/${id}`
-    );
-    return response.data;
+    const hsnCodeExist = await hsnCodeDao.getById(hsnCodeId);
+    if (!hsnCodeExist) {
+      const result = { success: false, message: 'HsnCode Id Not Exist' };
+      return result;
+    }
+    const data = await hsnCodeDao.deleteHsnCode(hsnCodeId);
+    if (data) {
+      const result = {
+        success: true,
+        message: 'HsnCode Data Deleted Successfully',
+      };
+      return result;
+    } else {
+      const result = {
+        success: false,
+        message: 'Failed to delete this hsnCode',
+      };
+      return result;
+    }
   } catch (error) {
-    console.log('Error in occur in delete hsc code :', error);
+    console.log('Error occurred in deleteHsnCode hsnCode service : ', error);
     throw error;
   }
 };
 
-const createHsnCode = async (values: JSON) => {
+/**
+ * Method to get HsnCode By code
+ * @param code
+ * @returns
+ */
+const getByCode = async (code: string) => {
   try {
-    const response = await axiosinterceptor.post(
-      `${environment.apiUrl}/hsn-code/`,
-      values
-    );
-    return response.data;
+    let result = null;
+    const hsnCodeData = await hsnCodeDao.getByCode(code);
+    if (hsnCodeData) {
+      result = { success: true, is_exist: true, data: hsnCodeData };
+      return result;
+    } else {
+      result = { success: false, is_exist: false };
+      return result;
+    }
   } catch (error) {
-    console.log('Error in post gst api :', error);
+    console.log('Error occurred in getByCode hsnCode service : ', error);
     throw error;
   }
 };
 
-const getOneHsnCode = async (id: number) => {
-  try {
-    const response = await axiosinterceptor.get(
-      `${environment.apiUrl}/hsn-code/get/${id}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log('Error in get by id hsn code data:', error);
-    throw error;
-  }
-};
-
-const updateHsnCode = async (values: JSON) => {
-  try {
-    const response = await axiosinterceptor.put(
-      `${environment.apiUrl}/hsn-code/`,
-      values
-    );
-    return response.data;
-  } catch (error) {
-    console.log('Error in update hsc-code api :', error);
-  }
-};
-const getByHsnCode = async (code: string) => {
-  try {
-    const response = await axiosinterceptor.get(
-      `${environment.apiUrl}/hsn-code/getByCode/${code}`
-    );
-    return response.data;
-  } catch (error) {
-    console.log('Error in get by id hsn code data:', error);
-    throw error;
-  }
-};
-
-export default {
-  getAllHsnCode,
-  deleteHsnCode,
+export {
   createHsnCode,
-  getOneHsnCode,
   updateHsnCode,
-  getByHsnCode,
+  getAllHsnCode,
+  getById,
+  deleteHsnCode,
+  getByCode,
 };
