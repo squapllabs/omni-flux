31083aba66eadb194b01d70d3a8616c490bd4e14
0054 <sanmuganathan.yuvaraj@aalamsoft.com>
2023-07-26 17:02:28 +0530

    loader implemented and build as component

--- apps/web/src/component/subSubCategory/subSubList.tsx
+++ apps/web/src/component/subSubCategory/subSubList.tsx
@@ -8,6 +8,7 @@ import MySnackbar from '../ui/MySnackbar';
 import {
   useGetAllSubSubcategory,
   useDeleteSubSubcategory,
+  getBySearchSubSubCategroy,
 } from '../../hooks/subSubCategory-hooks';
 import CustomDialog from '../ui/customDialog';
 import SubSubForm from './subSubForm';
@@ -21,9 +22,16 @@ import { createSubSubcategory } from '../../hooks/subSubCategory-hooks';
 import * as Yup from 'yup';
 import Select from '../ui/Select';
 import SearchIcon from '../menu/icons/search';
+import CustomLoader from '../ui/customLoader';
 
 const SubSubCategoryList = () => {
-  const { data: getAllSubSubCategory, isLoading: loader } = useGetAllSubSubcategory();
+  const { data: getAllSubSubCategory, isLoading: loader } =
+    useGetAllSubSubcategory();
+  const {
+    mutate: postFilterRequest,
+    data: getFilterData,
+    isLoading: filterLoading,
+  } = getBySearchSubSubCategroy();
   const { data: getAllSubCategory = [] } = useGetAllSubcategoryDrop();
   const { mutate: getDeleteSubSubCategoryByID } = useDeleteSubSubcategory();
   const [value, setValue] = useState(0);
@@ -37,6 +45,11 @@ const SubSubCategoryList = () => {
   const { mutate: createNewSubSubCategory } = createSubSubcategory();
   const validationSchema = getCreateValidateyup(Yup);
   const [selectedValue, setSelectedValue] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState(false);
+  const [filterValues, setFilterValues] = useState({
+    search_by_name: '',
+  });
   const [initialValues, setInitialValues] = useState({
     sub_sub_category_id: '',
     name: '',
@@ -166,7 +179,32 @@ const SubSubCategoryList = () => {
       },
     },
   ];
-
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValues({
+      ...filterValues,
+      ['search_by_name']: event.target.value,
+    });
+  };
+  const handleSearch = async () => {
+    let demo: any = {
+      offset: 0,
+      limit: 3,
+      order_by_column: 'updated_date',
+      order_by_direction: 'asc',
+      status: 'AC',
+      ...filterValues,
+    };
+    postFilterRequest(demo);
+    setIsLoading(false);
+    setFilter(true);
+  };
+  const handleReset = async () => {
+    setFilter(false);
+    setFilterValues({
+      search_by_name: '',
+    });
+    setIsLoading(false);
+  };
   const options = {
     filter: false,
     search: true,
@@ -174,6 +212,7 @@ const SubSubCategoryList = () => {
     print: false,
     download: false,
     viewColumns: false,
+    Search: false,
     selectableRows: 'none' as const,
     textLabels: {
       body: {
@@ -212,135 +251,159 @@ const SubSubCategoryList = () => {
   });
 
   return (
-    <div className={Styles.container}>
-      <div className={Styles.textContent}>
-        <h3>Add New Sub Sub Categories</h3>
-        <span className={Styles.content}>
-          Manage your raw materials (Raw, Semi Furnished & Finished).
-        </span>
-      </div>
-      <form onSubmit={formik.handleSubmit}>
-        <div className={Styles.fields}>
-          <div className={Styles.projectField}>
-            <span className={Styles.projectHeading}>Sub Category</span>
-            <Select
-              options={getAllSubCategory}
-              onChange={handleDropdownChange}
-              value={selectedValue}
-              defaultLabel="Select from options"
-              width="100%"
-            />
-            {formik.touched.sub_category_id &&
-              formik.errors.sub_category_id && (
-                <div className={Styles.error}>
-                  {formik.errors.sub_category_id}
-                </div>
-              )}
-          </div>
-          <div>
-            <Input
-              name="name"
-              label="Sub sub Category Name"
-              placeholder="Sub sub category name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && formik.errors.name}
-              width="100%"
-            />
-          </div>
-          <div>
-            <Input
-              name="budget"
-              label="Budget"
-              placeholder="Enter budget"
-              value={formik.values.budget}
-              onChange={formik.handleChange}
-              error={formik.touched.budget && formik.errors.budget}
-            />
-          </div>
-          <div>
-            <Button
-              text="Add New Sub Sub Category"
-              backgroundColor="#7F56D9"
-              fontSize={14}
-              fontWeight={500}
-              width={135}
-            />
-          </div>
-        </div>
-      </form>
-      <div className={Styles.textContent}>
-        <h3>List of Sub Sub Categories</h3>
-        <span className={Styles.content}>
-          Manage your raw materials (Raw, Semi Furnished & Finished).
-        </span>
-      </div>
-      <div className={Styles.searchField}>
+    <div>
+      <CustomLoader
+        loading={isLoading === true ? loader : filterLoading}
+        // loading={true}
+        size={48}
+        color="#333C44"
+      >
         <div>
-          <Input
-            name="budget"
-            placeholder="Search by item name"
-            width="160%"
-            prefixIcon={<SearchIcon />}
-          />
+          <div className={Styles.box}>
+            <div className={Styles.textContent}>
+              <h3>Add New Sub Sub Categories</h3>
+              <span className={Styles.content}>
+                Manage your raw materials (Raw, Semi Furnished & Finished).
+              </span>
+            </div>
+            <form onSubmit={formik.handleSubmit}>
+              <div className={Styles.fields}>
+                <div className={Styles.projectField}>
+                  <span className={Styles.projectHeading}>Sub Category</span>
+                  <Select
+                    options={getAllSubCategory}
+                    onChange={handleDropdownChange}
+                    value={selectedValue}
+                    defaultLabel="Select from options"
+                    width="100%"
+                  />
+                  {formik.touched.sub_category_id &&
+                    formik.errors.sub_category_id && (
+                      <div className={Styles.error}>
+                        {formik.errors.sub_category_id}
+                      </div>
+                    )}
+                </div>
+                <div>
+                  <Input
+                    name="name"
+                    label="Sub sub Category Name"
+                    placeholder="Sub sub category name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    error={formik.touched.name && formik.errors.name}
+                    width="100%"
+                  />
+                </div>
+                <div>
+                  <Input
+                    name="budget"
+                    label="Budget"
+                    placeholder="Enter budget"
+                    value={formik.values.budget}
+                    onChange={formik.handleChange}
+                    error={formik.touched.budget && formik.errors.budget}
+                  />
+                </div>
+                <div>
+                  <Button
+                    text="Add New Sub Sub Category"
+                    backgroundColor="#7F56D9"
+                    fontSize={14}
+                    fontWeight={500}
+                    width={135}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className={Styles.box}>
+            <div className={Styles.textContent}>
+              <h3>List of Sub Sub Categories</h3>
+              <span className={Styles.content}>
+                Manage your raw materials (Raw, Semi Furnished & Finished).
+              </span>
+            </div>
+            <div className={Styles.searchField}>
+              <div>
+                <Input
+                  name="budget"
+                  placeholder="Search by item name"
+                  width="160%"
+                  prefixIcon={<SearchIcon />}
+                  name="search_by_name"
+                  value={filterValues.search_by_name}
+                  onChange={(e) => handleFilterChange(e)}
+                />
+              </div>
+              <div className={Styles.searchButton}>
+                <Button
+                  text="Search"
+                  fontSize={14}
+                  fontWeight={500}
+                  width={120}
+                  backgroundColor="#F6F4EB"
+                  textColor="#7F56D9"
+                  onClick={handleSearch}
+                />
+                <Button
+                  text="Reset"
+                  fontSize={14}
+                  fontWeight={500}
+                  width={120}
+                  backgroundColor="white"
+                  textColor="#B2B2B2"
+                  onClick={handleReset}
+                />
+              </div>
+            </div>
+            <div className={Styles.tableContainer}>
+              <MUIDataTable
+                title=""
+                columns={columns}
+                options={options}
+                data={getAllSubSubCategory}
+                data={
+                  filter === true
+                    ? getFilterData?.content
+                    : getAllSubSubCategory
+                }
+              />
+            </div>
+          </div>
         </div>
-        <div className={Styles.searchButton}>
-          <Button
-            text="Search"
-            fontSize={14}
-            fontWeight={500}
-            width={120}
-            backgroundColor="#F6F4EB"
-            textColor="#7F56D9"
-          />
-          <Button
-            text="Reset"
-            fontSize={14}
-            fontWeight={500}
-            width={120}
-            backgroundColor="white"
-            textColor="#B2B2B2"
-          />
-        </div>
-      </div>
-      <div className={Styles.tableContainer}>
-        <MUIDataTable
-          title={'Sub Sub Categories List'}
-          columns={columns}
-          options={options}
-          data={getAllSubSubCategory}
+
+        <CustomDialog
+          open={open}
+          handleClose={handleClose}
+          title="Delete Sub Sub Category"
+          content="Are you want to delete this category?"
+          handleConfirm={deleteSubSubCategory}
         />
-      </div>
-      <CustomDialog
-        open={open}
-        handleClose={handleClose}
-        title="Delete Sub Sub Category"
-        content="Are you want to delete this category?"
-        handleConfirm={deleteSubSubCategory}
-      />
-      <MySnackbar
-        open={openSnack}
-        message={message}
-        onClose={handleSnackBarClose}
-        severity={'success'}
-        autoHideDuration={1000}
-      />
-      <CustomDialogBox
-        open={openPopup}
-        handleClose={handleClosePopup}
-        title="Sub Sub Category Creation"
-        content={
-          <SubSubForm
-            setOpenPopup={setOpenPopup}
-            open={openPopup}
-            setReload={setReload}
-            mode={mode}
-            subSubCategoryId={subSubCategoryId}
-            setOpenSnack={setOpenSnack}
-            setMessage={setMessage}
-          />
-        }
-      />
+        <MySnackbar
+          open={openSnack}
+          message={message}
+          onClose={handleSnackBarClose}
+          severity={'success'}
+          autoHideDuration={1000}
+        />
+        <CustomDialogBox
+          open={openPopup}
+          handleClose={handleClosePopup}
+          title="Sub Sub Category Creation"
+          content={
+            <SubSubForm
+              setOpenPopup={setOpenPopup}
+              open={openPopup}
+              setReload={setReload}
+              mode={mode}
+              subSubCategoryId={subSubCategoryId}
+              setOpenSnack={setOpenSnack}
+              setMessage={setMessage}
+            />
+          }
+        />
+      </CustomLoader>
     </div>
   );
 };
