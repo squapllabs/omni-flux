3bd3a97db6e55ae89f7380d090638c833f8a39cc
Vijay Perumalsamy <0065@aalamsoft.local>
2023-09-13 19:19:02 +0530

    DB Script and Postman Collection

--- db-script/September-2023/script_12-09-23.sql
+++ db-script/September-2023/script_12-09-23.sql
@@ -69,3 +69,44 @@ add column project_id int4;
 
 alter table indent_request 
 add constraint fk_indent_request_project_id foreign key (project_id) references project(project_id);
+
+create table purchase_request(
+purchase_request_id serial4 not null,
+indent_request_id int4 not null,
+requester_user_id int4 not null,
+request_date date,
+status varchar(50),
+vendor_selection_method varchar(50),
+project_id int4,
+selected_vendor_id int4,
+total_cost double precision,
+is_delete boolean not null default false,
+created_date timestamptz not null default now(),
+updated_date timestamptz not null,
+created_by int4,
+updated_by int4,
+constraint pk_purchase_request primary key (purchase_request_id),
+constraint fk_purchase_request_indent_request_id foreign key (indent_request_id) references indent_request(indent_request_id),
+constraint fk_purchase_request_requester_user_id foreign key (requester_user_id) references users(user_id),
+constraint fk_purchase_request_project_id foreign key (project_id) references project(project_id),
+constraint fk_purchase_request_selected_vendor_id foreign key (selected_vendor_id) references vendor(vendor_id)
+)
+
+create table vendor_quotes(
+vendor_quotes_id serial4 not null,
+vendor_id int4 not null,
+purchase_request_id int4 not null,
+quotation_date date,
+quotation_status varchar(50),
+total_quotation_amount double precision,
+remarks varchar(100),
+quatation_details jsonb,
+is_delete boolean not null default false,
+created_date timestamptz not null default now(),
+updated_date timestamptz not null,
+created_by int4,
+updated_by int4,
+constraint pk_vendor_quotes primary key (vendor_quotes_id),
+constraint fk_vendor_quotes_vendor_id foreign key (vendor_id) references vendor(vendor_id),
+constraint fk_vendor_quotes_purchase_request_id foreign key (purchase_request_id) references purchase_request(purchase_request_id)
+)
