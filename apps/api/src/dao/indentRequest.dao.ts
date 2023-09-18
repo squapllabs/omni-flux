7beff970ada6efd2cf2dda04df7340e966b4db55
Vijay Perumalsamy <0065@aalamsoft.local>
2023-09-18 17:43:27 +0530

    Transaction Implemented in Purchase Order ,Indent Request and Project

--- apps/api/src/dao/indentRequest.dao.ts
+++ apps/api/src/dao/indentRequest.dao.ts
@@ -34,59 +34,73 @@ const add = async (
       ? new Date(rejected_date)
       : null;
     const transaction = connectionObj !== null ? connectionObj : prisma;
-    const indentRequest = await transaction.indent_request.create({
-      data: {
-        requester_user_id,
-        requested_date: formatted_requested_date,
-        request_status,
-        priority,
-        description,
-        expected_delivery_date: formatted_expected_delivery_date,
-        total_cost,
-        approver_user_id,
-        approver_status,
-        approved_date: formatted_approved_date,
-        rejected_date: formatted_rejected_date,
-        approver_comments,
-        project_id,
-        created_by,
-        created_date: currentDate,
-        updated_date: currentDate,
-        is_delete: is_delete,
-      },
-    });
 
-    const new_indent_request_id = indentRequest?.indent_request_id;
-    const indentRequestDetailsData = [];
+    const result = await transaction
+      .$transaction(async (tx) => {
+        const indentRequest = await tx.indent_request.create({
+          data: {
+            requester_user_id,
+            requested_date: formatted_requested_date,
+            request_status,
+            priority,
+            description,
+            expected_delivery_date: formatted_expected_delivery_date,
+            total_cost,
+            approver_user_id,
+            approver_status,
+            approved_date: formatted_approved_date,
+            rejected_date: formatted_rejected_date,
+            approver_comments,
+            project_id,
+            created_by,
+            created_date: currentDate,
+            updated_date: currentDate,
+            is_delete: is_delete,
+          },
+        });
 
-    for (const indent_request_detail of indent_request_details) {
-      const bom_detail_id = indent_request_detail.bom_detail_id;
-      const quantity = indent_request_detail.quantity;
-      const total = indent_request_detail.total;
-      const is_delete = indent_request_detail.is_delete;
-      if (is_delete === false) {
-        const indentRequestDetails =
-          await transaction.indent_request_details.create({
-            data: {
-              indent_request_id: new_indent_request_id,
-              bom_detail_id,
-              quantity,
-              total,
-              created_by,
-              created_date: currentDate,
-              updated_date: currentDate,
-              is_delete: false,
-            },
-          });
-        indentRequestDetailsData.push(indentRequestDetails);
-      }
-    }
+        const new_indent_request_id = indentRequest?.indent_request_id;
+        const indentRequestDetailsData = [];
 
-    const result = {
-      indent_request: indentRequest,
-      indent_request_details: indentRequestDetailsData,
-    };
+        for (const indent_request_detail of indent_request_details) {
+          const bom_detail_id = indent_request_detail.bom_detail_id;
+          const quantity = indent_request_detail.quantity;
+          const total = indent_request_detail.total;
+          const is_delete = indent_request_detail.is_delete;
+          if (is_delete === false) {
+            const indentRequestDetails = await tx.indent_request_details.create(
+              {
+                data: {
+                  indent_request_id: new_indent_request_id,
+                  bom_detail_id,
+                  quantity,
+                  total,
+                  created_by,
+                  created_date: currentDate,
+                  updated_date: currentDate,
+                  is_delete: false,
+                },
+              }
+            );
+            indentRequestDetailsData.push(indentRequestDetails);
+          }
+        }
 
+        const result = {
+          indent_request: indentRequest,
+          indent_request_details: indentRequestDetailsData,
+        };
+
+        return result;
+      })
+      .then((data) => {
+        console.log('Successfully Indent Request Data Returned ', data);
+        return data;
+      })
+      .catch((error: string) => {
+        console.log('Failure, ROLLBACK was executed', error);
+        throw error;
+      });
     return result;
   } catch (error) {
     console.log('Error occurred in indentRequestDao add', error);
@@ -128,84 +142,100 @@ const edit = async (
       ? new Date(rejected_date)
       : null;
     const transaction = connectionObj !== null ? connectionObj : prisma;
-    const indentRequest = await transaction.indent_request.update({
-      where: {
-        indent_request_id: indent_request_id,
-      },
-      data: {
-        requester_user_id,
-        requested_date: formatted_requested_date,
-        request_status,
-        priority,
-        description,
-        expected_delivery_date: formatted_expected_delivery_date,
-        total_cost,
-        approver_user_id,
-        approver_status,
-        approved_date: formatted_approved_date,
-        rejected_date: formatted_rejected_date,
-        approver_comments,
-        project_id,
-        updated_by,
-        updated_date: currentDate,
-      },
-    });
-    const indentRequestDetailsData = [];
 
-    for (const indent_request_detail of indent_request_details) {
-      const bom_detail_id = indent_request_detail.bom_detail_id;
-      const quantity = indent_request_detail.quantity;
-      const total = indent_request_detail.total;
-      const is_delete = indent_request_detail.is_delete;
-      const indent_request_details_id =
-        indent_request_detail.indent_request_details_id;
+    const result = await transaction
+      .$transaction(async (tx) => {
+        const indentRequest = await tx.indent_request.update({
+          where: {
+            indent_request_id: indent_request_id,
+          },
+          data: {
+            requester_user_id,
+            requested_date: formatted_requested_date,
+            request_status,
+            priority,
+            description,
+            expected_delivery_date: formatted_expected_delivery_date,
+            total_cost,
+            approver_user_id,
+            approver_status,
+            approved_date: formatted_approved_date,
+            rejected_date: formatted_rejected_date,
+            approver_comments,
+            project_id,
+            updated_by,
+            updated_date: currentDate,
+          },
+        });
+        const indentRequestDetailsData = [];
 
-      if (indent_request_details_id) {
-        if (is_delete === false) {
-          const indentRequestDetails =
-            await transaction.indent_request_details.update({
-              where: { indent_request_details_id: indent_request_details_id },
-              data: {
-                indent_request_id,
-                bom_detail_id,
-                quantity,
-                total,
-                updated_by,
-                updated_date: currentDate,
-              },
-            });
-          indentRequestDetailsData.push(indentRequestDetails);
-        } else if (is_delete === true) {
-          await transaction.indent_request_details.update({
-            where: { indent_request_details_id: indent_request_details_id },
-            data: {
-              is_delete: true,
-            },
-          });
+        for (const indent_request_detail of indent_request_details) {
+          const bom_detail_id = indent_request_detail.bom_detail_id;
+          const quantity = indent_request_detail.quantity;
+          const total = indent_request_detail.total;
+          const is_delete = indent_request_detail.is_delete;
+          const indent_request_details_id =
+            indent_request_detail.indent_request_details_id;
+
+          if (indent_request_details_id) {
+            if (is_delete === false) {
+              const indentRequestDetails =
+                await tx.indent_request_details.update({
+                  where: {
+                    indent_request_details_id: indent_request_details_id,
+                  },
+                  data: {
+                    indent_request_id,
+                    bom_detail_id,
+                    quantity,
+                    total,
+                    updated_by,
+                    updated_date: currentDate,
+                  },
+                });
+              indentRequestDetailsData.push(indentRequestDetails);
+            } else if (is_delete === true) {
+              await tx.indent_request_details.update({
+                where: { indent_request_details_id: indent_request_details_id },
+                data: {
+                  is_delete: true,
+                },
+              });
+            }
+          } else if (is_delete === false) {
+            const indentRequestDetails = await tx.indent_request_details.create(
+              {
+                data: {
+                  indent_request_id,
+                  bom_detail_id,
+                  quantity,
+                  total,
+                  created_by: updated_by,
+                  created_date: currentDate,
+                  updated_date: currentDate,
+                  is_delete: false,
+                },
+              }
+            );
+            indentRequestDetailsData.push(indentRequestDetails);
+          }
         }
-      } else if (is_delete === false) {
-        const indentRequestDetails =
-          await transaction.indent_request_details.create({
-            data: {
-              indent_request_id,
-              bom_detail_id,
-              quantity,
-              total,
-              created_by: updated_by,
-              created_date: currentDate,
-              updated_date: currentDate,
-              is_delete: false,
-            },
-          });
-        indentRequestDetailsData.push(indentRequestDetails);
-      }
-    }
 
-    const result = {
-      indent_request: indentRequest,
-      indent_request_details: indentRequestDetailsData,
-    };
+        const result = {
+          indent_request: indentRequest,
+          indent_request_details: indentRequestDetailsData,
+        };
 
+        return result;
+      })
+      .then((data) => {
+        console.log('Successfully Indent Request Data Returned ', data);
+        return data;
+      })
+      .catch((error: string) => {
+        console.log('Failure, ROLLBACK was executed', error);
+        throw error;
+      });
     return result;
   } catch (error) {
     console.log('Error occurred in indentRequestDao edit', error);
--- apps/api/src/dao/purchaseOrder.dao.ts
+++ apps/api/src/dao/purchaseOrder.dao.ts
@@ -194,61 +194,77 @@ const createPurchaseOrderWithItem = async (
   purchase_order_item,
   connectionObj = null
 ) => {
+  let transaction;
   try {
     const currentDate = new Date();
     const is_delete = false;
     const formatted_order_date = order_date ? new Date(order_date) : null;
-    const transaction = connectionObj !== null ? connectionObj : prisma;
-    const purchaseOrder = await transaction.purchase_order.create({
-      data: {
-        purchase_request_id,
-        vendor_id,
-        order_date: formatted_order_date,
-        status,
-        total_cost,
-        order_remark,
-        created_by,
-        created_date: currentDate,
-        updated_date: currentDate,
-        is_delete: is_delete,
-      },
-    });
+    transaction = connectionObj !== null ? connectionObj : prisma;
 
-    const new_purchase_order_id = purchaseOrder?.purchase_order_id;
-    const purchaseOrderItemDetails = [];
-    if (purchase_order_item.length > 0) {
-      for (const value of purchase_order_item) {
-        const item_id = value.item_id;
-        const order_quantity = value.order_quantity;
-        const unit_price = value.unit_price;
-
-        const purchaseOrderItem = await transaction.purchase_order_item.create({
+    const result = await transaction
+      .$transaction(async (tx) => {
+        const purchaseOrder = await tx.purchase_order.create({
           data: {
-            purchase_order_id: new_purchase_order_id,
-            item_id,
-            order_quantity,
-            unit_price,
+            purchase_request_id,
+            vendor_id,
+            order_date: formatted_order_date,
+            status,
+            total_cost,
+            order_remark,
             created_by,
             created_date: currentDate,
             updated_date: currentDate,
             is_delete: is_delete,
           },
         });
-        purchaseOrderItemDetails.push(purchaseOrderItem);
-      }
-    }
 
-    const purchaseOrderData = {
-      purchase_order: purchaseOrder,
-      purchase_order_item: purchaseOrderItemDetails,
-    };
+        const new_purchase_order_id = purchaseOrder?.purchase_order_id;
+        const purchaseOrderItemDetails = [];
 
-    return purchaseOrderData;
+        if (purchase_order_item.length > 0) {
+          for (const value of purchase_order_item) {
+            const item_id = value.item_id;
+            const order_quantity = value.order_quantity;
+            const unit_price = value.unit_price;
+
+            const purchaseOrderItem = await tx.purchase_order_item.create({
+              data: {
+                purchase_order_id: new_purchase_order_id,
+                item_id,
+                order_quantity,
+                unit_price,
+                created_by,
+                created_date: currentDate,
+                updated_date: currentDate,
+                is_delete: is_delete,
+              },
+            });
+            purchaseOrderItemDetails.push(purchaseOrderItem);
+          }
+        }
+
+        const purchaseOrderData = {
+          purchase_order: purchaseOrder,
+          purchase_order_item: purchaseOrderItemDetails,
+        };
+
+        return purchaseOrderData;
+      })
+      .then((data) => {
+        console.log('Successfully Purchase Order Data Returned ', data);
+        return data;
+      })
+      .catch((error: string) => {
+        console.log('Failure, ROLLBACK was executed', error);
+        throw error;
+      });
+    return result;
   } catch (error) {
     console.log(
       'Error occurred in purchaseOrderDao createPurchaseOrderWithItem',
       error
     );
+
     throw error;
   }
 };
