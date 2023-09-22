3fdad7f3fc5f0d59f5379149b6272111fc6d6c77
Sathyam <sathyam@squapltechnologies.com>
2023-09-22 19:03:34 +0530

    category problem fixed

--- apps/web/src/component/settings/homeSetting.tsx
+++ apps/web/src/component/settings/homeSetting.tsx
@@ -2,37 +2,79 @@ import React, { useState } from 'react';
 import Styles from '../../styles/homeSettings.module.scss';
 import CustomGroupButton from '../ui/CustomGroupButton';
 import Category from '../category/category';
+import SubCategoryList from '../subCategory/subCategoryList';
 import UserList from '../users/userList';
 import MasterData from '../masterData/masterData';
+import ProjectList from '../project/projectList';
 import LeadList from '../leadEnquires/leadList';
+import MachineryList from '../machinery/machineryList';
 import VendorList from '../vendor/vendorList';
 import LabourList from '../labour/labourList';
-import PurchaseList from '../purchaseApproval/purchaseList';
-import PlanEngApproval from '../indentApproval/indentList';
-import { store, RootState } from '../../redux/store';
-import { getToken } from '../../redux/reducer';
+import CategoryList from '../category/categoryList';
+import SubSubCategoryList from '../subSubCategory/subSubList';
+import GstList from '../gst/gstList';
+import UomList from '../uom/uomList';
+import ClientList from '../client/clientList';
+import HsnCodeList from '../hsnCode/hsnCodeList';
+import SideNav from '../ui/SubmenuNav';
 const Settings = () => {
-  const state: RootState = store.getState();
-  const encryptedData = getToken(state, 'Data');
-  const roleName = encryptedData?.userData?.user_roles[0]?.role_data?.role_name;
-
-  const menuItems = [
-    { label: 'Category', value: 'C' },
+  const [buttonLabels, setButtonLabels] = useState([
     { label: 'Master Data', value: 'M' },
+    { label: 'Category', value: 'C' },
     { label: 'User List', value: 'U' },
+    { label: 'Project List', value: 'PL' },
     { label: 'Lead List', value: 'LL' },
+    { label: 'Machinery List', value: 'ML' },
     { label: 'Vendor List', value: 'VL' },
     { label: 'Labour List', value: 'LB' },
-  ];
+  ]);
+  const [selectedItem, setSelectedItem] = useState<number>(1);
+  console.log(selectedItem);
+  const menuItems = [
+    {
+      id: 1,
+      name: 'Category',
+    },
+    {
+      id: 2,
+      name: 'Sub Category',
+    },
+    {
+      id: 3,
+      name: 'Sub Sub Category',
+    },
 
-  if (roleName === 'Planning Engineer') {
-    menuItems.push({ label: 'Indent Approval', value: 'IA' });
-  }
-  if (roleName === 'Purchase Manager') {
-    menuItems.push({ label: 'Purchase List', value: 'PL' });
-  }
-  const [buttonLabels, setButtonLabels] = useState(menuItems);
-  const [activeButton, setActiveButton] = useState<string | null>('C');
+    {
+      id: 4,
+      name: 'GST',
+    },
+    {
+      id: 5,
+      name: 'UOM',
+    },
+    {
+      id: 6,
+      name: 'Client',
+    },
+    {
+      id: 7,
+      name: 'HSN Code',
+    },
+  ];
+  const handleMenuItemClick = (id: number) => {
+    setSelectedItem(id);
+  };
+  const mainContentComponents: { [key: number]: JSX.Element } = {
+    1: <CategoryList />,
+    2: <SubCategoryList />,
+    3: <SubSubCategoryList />,
+    4: <GstList />,
+    5: <UomList />,
+    6: <ClientList />,
+    7: <HsnCodeList />,
+  };
+
+  const [activeButton, setActiveButton] = useState<string | null>('M');
   const handleGroupButtonClick = (value: string) => {
     setActiveButton(value);
   };
@@ -46,19 +88,28 @@ const Settings = () => {
             onClick={handleGroupButtonClick}
             activeButton={activeButton}
           />
+          {activeButton == 'C' && (
+            <SideNav
+              menuItems={menuItems}
+              selectedItem={selectedItem}
+              handleMenuItemClick={handleMenuItemClick}
+            />
+          )}
         </div>
       </div>
       <div className={Styles.dividerLine}></div>
-      <div>
-        {activeButton === 'C' && <Category />}
-        {activeButton === 'M' && <MasterData />}
-        {activeButton === 'U' && <UserList />}
-        {activeButton === 'LL' && <LeadList />}
-        {activeButton === 'VL' && <VendorList />}
-        {activeButton === 'LB' && <LabourList />}
-        {activeButton === 'IA' && <PlanEngApproval />}
-        {activeButton === 'PL' && <PurchaseList />}
-      </div>
+      <div></div>
+      {activeButton === 'C' && <Category selectedItem={selectedItem} />}
+
+      {activeButton === 'M' && <MasterData />}
+      {activeButton === 'U' && <UserList />}
+      {activeButton === 'PL' && <ProjectList />}
+      {activeButton === 'LL' && <LeadList />}
+      {activeButton === 'ML' && <MachineryList />}
+      {activeButton === 'VL' && <VendorList />}
+      {activeButton === 'LB' && <LabourList />}
+      {/* {activeButton === 'IA' && <PlanEngApproval />} */}
+      {/* {activeButton === 'PL' && <PurchaseList />} */}
     </div>
   );
 };
