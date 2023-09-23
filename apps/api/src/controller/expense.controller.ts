595cb822d28cfff06869f4d1ec8fc075d8ba54fd
Vijay Perumalsamy <0065@aalamsoft.local>
2023-09-23 13:51:24 +0530

    Expense Along with Expense Details CRUD API

---
+++ apps/api/src/controller/expense.controller.ts
@@ -0,0 +1,99 @@
+import catchAsync from '../utils/catchAsync';
+import * as expenseService from '../services/expense.service';
+import { handleError, ErrorHandler } from '../config/error';
+const errorText = 'Error';
+
+const createExpense = catchAsync(async (req, res) => {
+  const methodName = '/createExpense';
+  try {
+    const expense = await expenseService.createExpense(req.body);
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const updateExpense = catchAsync(async (req, res) => {
+  const methodName = '/updateExpense';
+  try {
+    const expense = await expenseService.updateExpense(req.body);
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const getAllExpense = catchAsync(async (req, res) => {
+  const methodName = '/getAllExpense';
+  try {
+    const expense = await expenseService.getAllExpense();
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const getByExpenseId = catchAsync(async (req, res) => {
+  const methodName = '/getByExpenseId';
+  try {
+    const expense = await expenseService.getById(req.params.expense_id);
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const deleteByExpenseId = catchAsync(async (req, res) => {
+  const methodName = '/deleteByExpenseId';
+  try {
+    const expense = await expenseService.deleteExpense(req.params.expense_id);
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const searchExpense = catchAsync(async (req, res) => {
+  const methodName = '/searchExpense';
+  try {
+    const expense = await expenseService.searchExpense(req.body);
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const getByProjectIdAndSiteId = catchAsync(async (req, res) => {
+  const methodName = '/getByProjectIdAndSiteId';
+  try {
+    const expense = await expenseService.getByProjectIdAndSiteId(
+      req.params.project_id,
+      req.params.site_id
+    );
+    res.send(expense);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+
+const getExpenseDetailsByExpenseId = catchAsync(async (req, res) => {
+  const methodName = '/getExpenseDetailsExpenseId';
+  try {
+    const expenseDetails = await expenseService.getExpenseDetailsByExpenseId(
+      req.params.expense_id
+    );
+    res.send(expenseDetails);
+  } catch (err) {
+    handleError(new ErrorHandler(errorText, methodName, err), res);
+  }
+});
+export {
+  createExpense,
+  updateExpense,
+  getAllExpense,
+  getByExpenseId,
+  deleteByExpenseId,
+  searchExpense,
+  getByProjectIdAndSiteId,
+  getExpenseDetailsByExpenseId,
+};
--- apps/web/src/environment/environment.ts
+++ apps/web/src/environment/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    development: true,
-    auth_secretKey: "lksikejdlkjoijlajljljoiojlsmsmn",
-    password_reset_secretKey:"cdyu2dfg4yuds&gcy587ydbdcduyudu",
-    // apiUrl: "http://localhost:8080/api",
-    // apiUrl: "http://192.168.2.27:8080/api",
-    apiUrl: "http://global.aalamsoft.com:8082/api",
-    LOCALE:'en-IN',   //country format
-    CURRENCY:'INR',   // currency symbol
-    INPUTBUDGET:'INR',  //input budget (label)
-    OUTPUTBUDGET:'INR',  //output budget (label)
-    NULLVALUE:'N/A' // null value (label)
+  development: true,
+  auth_secretKey: 'lksikejdlkjoijlajljljoiojlsmsmn',
+  password_reset_secretKey: 'cdyu2dfg4yuds&gcy587ydbdcduyudu',
+  apiUrl: 'http://localhost:8080/api',
+  // apiUrl: "http://192.168.2.27:8080/api",
+  // apiUrl: "http://global.aalamsoft.com:8082/api",
+  LOCALE: 'en-IN', //country format
+  CURRENCY: 'INR', // currency symbol
+  INPUTBUDGET: 'INR', //input budget (label)
+  OUTPUTBUDGET: 'INR', //output budget (label)
+  NULLVALUE: 'N/A', // null value (label)
 };
