64c96d14e0864d9c8be109122c36cb08516890d0
0054 <sanmuganathan.yuvaraj@aalamsoft.com>
2023-10-03 16:44:20 +0530

    code commite

---
+++ apps/web/src/component/project/projectSubheader.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Styles from '../../styles/expanses.module.scss';
+import PreviousPageIcon from '../menu/icons/previousPageIcon';
+import { useNavigate } from 'react-router-dom';
+interface CancelFilterIconProps {
+  width?: number;
+  height?: number;
+  color?: string;
+  style?: React.CSSProperties;
+  navigation: string;
+  title: string;
+  description: string;
+}
+
+const ProjectSubheader: React.FC<CancelFilterIconProps> = ({
+  width = 24,
+  height = 22,
+  color = 'currentColor',
+  style,
+  title,
+  navigation,
+  description,
+}) => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className={Styles.sub_header}>
+        <div
+          className={Styles.logo}
+          onClick={() => {
+            navigate(`${navigation}`);
+          }}
+        >
+          <PreviousPageIcon width={20} height={20} color="#7f56d9" />
+        </div>
+        <div style={{ padding: '8px', display: 'flex' }}>
+          <div className={Styles.vertical}>
+            <div className={Styles.verticalLine}></div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: '700px',
+          }}
+        >
+          <div className={Styles.textContent_1}>
+            <h3>{title}</h3>
+            <span className={Styles.content}>{description}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSubheader;
