be7a8c90502202f89d17d065bb4da9789df2d51f
0054 <sanmuganathan.yuvaraj@aalamsoft.com>
2023-11-15 14:39:37 +0530

    code commited

--- apps/web/src/component/menu/icons/lastPageIcon.tsx
+++ apps/web/src/component/menu/icons/lastPageIcon.tsx
@@ -24,14 +24,27 @@ const LastPageIcon: React.FC<LastPageProps> = ({
   };
 
   return (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleClick}       style={{
-      ...style,
-      opacity: disabled ? 0.5 : 1,
-      pointerEvents: disabled ? 'none' : 'auto',
-    }}>
-      <path d="M4.1665 9.99996H15.8332M15.8332 9.99996L9.99984 4.16663M15.8332 9.99996L9.99984 15.8333" stroke={color} stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={handleClick}
+      style={{
+        ...style,
+        opacity: disabled ? 0.5 : 1,
+        pointerEvents: disabled ? 'none' : 'auto',
+      }}
+    >
+      <path
+        d="M4.1665 9.99996H15.8332M15.8332 9.99996L9.99984 4.16663M15.8332 9.99996L9.99984 15.8333"
+        stroke={color}
+        strokeWidth="1.67"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
-
   );
 };
 
